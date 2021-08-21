ac54bbdf863fb22a9bfdc5dbf8b8fc49592560ba
Marlon Strong <tsenane14@gmail.com>
2021-08-22 01:39:26 +0200

    :wrench: renamed > emailForm->mainForm

--- client/int-blog/components/otpForm.js
+++ client/int-blog/components/otpForm.js
@@ -36,3 +36,73 @@ export default function OtpForm() {
       </form>
     )
 }
+
+
+const PasswordForm = () => {
+  return (
+    <form onSubmit={handleSubmitPass}>
+      <FormControl id="password">
+        <Flex justifyContent="space-around" flexDirection="column">
+          <Input
+            value={password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
+            variant="filled"
+            placeholder="nova password"
+          />
+          <Input
+            value={cPassword}
+            onChange={(e) => setCPassword(e.currentTarget.value)}
+            variant="filled"
+            placeholder="confirmar password"
+          />
+        </Flex>
+        <Flex mt="5" alignItems="center" justifyContent="center">
+          <Button
+            isLoading={fetching}
+            type="submit"
+            bgColor="green.light"
+            color="white"
+          >
+            Confirmar
+          </Button>
+        </Flex>
+      </FormControl>
+    </form>
+  );
+};
+
+const OtpForm = () => {
+  return (
+    <form onSubmit={handleSubmitOtp}>
+      <FormControl id="otp">
+        <Flex justifyContent="space-around" flexDirection="column">
+          <HStack>
+            <PinInput
+              manageFocus
+              onChange={(e) => setOtp(e)}
+              variant="filled"
+              value={otp}
+              otp
+              size="lg"
+            >
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+            </PinInput>
+          </HStack>
+        </Flex>
+        <Flex mt="5" alignItems="center" justifyContent="center">
+          <Button
+            isLoading={fetching}
+            type="submit"
+            bgColor="green.light"
+            color="white"
+          >
+            Confirmar
+          </Button>
+        </Flex>
+      </FormControl>
+    </form>
+  );
+};
--- client/int-blog/pages/auth/forgotPassword.js
+++ client/int-blog/pages/auth/forgotPassword.js
@@ -12,7 +12,7 @@ import {
 } from "@chakra-ui/react";
 import Image from "next/image";
 import React, { useState } from "react";
-import MainForm from "../../components/emailForm";
+import MainForm from "../../components/mainForm";
 
 import MainLink from "../../components/mainLink";
 import styles from "../../styles/login-st.module.css";
@@ -61,74 +61,7 @@ export default function ForgotPassword() {
     }, 2000);
   };
 
-  const PasswordForm = () => {
-    return (
-      <form onSubmit={handleSubmitPass}>
-        <FormControl id="password">
-          <Flex justifyContent="space-around" flexDirection="column">
-            <Input
-              value={password}
-              onChange={(e) => setPassword(e.currentTarget.value)}
-              variant="filled"
-              placeholder="nova password"
-            />
-            <Input
-              value={cPassword}
-              onChange={(e) => setCPassword(e.currentTarget.value)}
-              variant="filled"
-              placeholder="confirmar password"
-            />
-          </Flex>
-          <Flex mt="5" alignItems="center" justifyContent="center">
-            <Button
-              isLoading={fetching}
-              type="submit"
-              bgColor="green.light"
-              color="white"
-            >
-              Confirmar
-            </Button>
-          </Flex>
-        </FormControl>
-      </form>
-    );
-  };
-
-  const OtpForm = () => {
-    return (
-      <form onSubmit={handleSubmitOtp}>
-        <FormControl id="otp">
-          <Flex justifyContent="space-around" flexDirection="column">
-            <HStack>
-              <PinInput
-                manageFocus
-                onChange={(e) => setOtp(e)}
-                variant="filled"
-                value={otp}
-                otp
-                size="lg"
-              >
-                <PinInputField />
-                <PinInputField />
-                <PinInputField />
-                <PinInputField />
-              </PinInput>
-            </HStack>
-          </Flex>
-          <Flex mt="5" alignItems="center" justifyContent="center">
-            <Button
-              isLoading={fetching}
-              type="submit"
-              bgColor="green.light"
-              color="white"
-            >
-              Confirmar
-            </Button>
-          </Flex>
-        </FormControl>
-      </form>
-    );
-  };
+ 
 
   // if(next==='none'){
   //     return null
