8b48d4cdba48e51d321466579f0012b4ecbbad69
Marlon Strong <tsenane14@gmail.com>
2021-08-21 21:00:56 +0200

    adjusted responsiveness and added conditional authHeader render

--- client/int-blog/pages/_app.js
+++ client/int-blog/pages/_app.js
@@ -3,10 +3,25 @@ import "@fontsource/montserrat";
 // import "../styles/login-st.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
+import router, { useRouter } from "next/router";
+import AuthHeader from "../components/authHeader";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const routeHistory = useRouter();
+  const [route, setRoute] = useState(routeHistory.pathname);
+
+  useEffect(() => {
+    setRoute(routeHistory.pathname);
+  }, [routeHistory]);
+
   return (
     <ChakraProvider theme={theme}>
+      {/* {console.log(routeHistory.pathname)} */}
+
+     {/* //just render de authHeader if the user is at auth routes */}
+      {(route === "/auth/login"|| "/auth/register"  ) && <AuthHeader />}
+
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- client/int-blog/pages/auth/register.js
+++ client/int-blog/pages/auth/register.js
@@ -1,7 +1,129 @@
 import React from "react";
+import styles from "../../styles/register-st.module.css";
+
+import AuthHeader from "../../components/authHeader";
+import { Box } from "@chakra-ui/layout";
+import {
+  Button,
+  Flex,
+  FormControl,
+  Input,
+  Link,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import Image from "next/image";
 
 function Register() {
-  return <div></div>;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  };
+
+  return (
+    <div>
+      {/* <AuthHeader /> */}
+      <Box className={styles.blob}>
+        <Flex alignItems="center" justifyContent="center">
+          <Flex
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="space-evenly"
+          >
+            <Image
+              src={"/intressantLOGO.svg"}
+              width={150}
+              height={100}
+              alt="logoInt"
+            />
+            <Text textAlign="center" color="blue.dark" fontWeight="bold">
+              Bem vindo a maior Comunidade Mocambicana
+            </Text>
+            <Text mt="5" color="blue.dark">
+              Criar conta
+            </Text>
+            <Flex
+              mt="10"
+              mb="10"
+              w={["60%", "40%"]}
+              alignItems="center"
+              justifyContent="space-between"
+              direction="row"
+              flexDirection="row"
+            >
+              <Link>
+                <Image
+                  src={"/Facebook.svg"}
+                  width={20}
+                  height={20}
+                  alt="logoInt"
+                />
+              </Link>
+
+              <Link>
+                <Image
+                  src={"/Google.svg"}
+                  width={20}
+                  height={20}
+                  alt="logoInt"
+                />
+              </Link>
+
+              <Link>
+                <Image
+                  src={"/Github.svg"}
+                  width={20}
+                  height={20}
+                  alt="logoInt"
+                />
+              </Link>
+            </Flex>
+            <Flex w="100%" justifyContent="center">
+              <form onSubmit={handleSubmit}>
+                <FormControl id="email">
+                  <Flex justifyContent="space-around" flexDirection="column">
+                    <Input variant="filled" placeholder="Email" />
+                    <Input
+                      type="password"
+                      mt="5"
+                      variant="filled"
+                      placeholder="Create password"
+                    />
+                    <Input
+                      type="password"
+                      mt="5"
+                      variant="filled"
+                      placeholder="Confirm password"
+                    />
+                  </Flex>
+                  <Flex
+                    mt="5"
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <Text>Esquecei a senha</Text>
+                    <Button type="submit" bgColor="green.light" color="white">
+                      Entrar
+                    </Button>
+                  </Flex>
+                </FormControl>
+              </form>
+            </Flex>
+            <Flex
+              mt="10"
+              flexDirection="column"
+              justifyContent="space-around"
+              alignItems="center"
+            >
+              <Link mt="5" href="/auth/login">
+                Tenho conta
+              </Link>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Box>
+    </div>
+  );
 }
 
 export default Register;
