3b3211b09a5a729dd63be70d9b138c35949b6a78
Marlon Strong <tsenane14@gmail.com>
2021-08-21 04:38:13 +0200

    added customizing login

--- client/int-blog/pages/auth/login.js
+++ client/int-blog/pages/auth/login.js
@@ -1,11 +1,28 @@
 // import React from "react";
+import Image from "next/image";
+import styles from "../../styles/login-st.module.css";
 
 import AuthHeader from "../../components/authHeader";
+import { Box } from "@chakra-ui/layout";
 
 function Login() {
   return (
     <div>
       <AuthHeader />
+      <Box className={styles.blob}>
+          <Flex>
+    <Box>
+        
+    </Box>
+          </Flex>
+      </Box>
+      {/* <Image
+        className="blob"
+        src="/blob.svg"
+        width="100%"
+        height="100%"
+        layout="responsive"
+      /> */}
     </div>
   );
 }
