206f5df43c0d6b9926363f0f3908140d1f0e108f
Marlon Strong <tsenane14@gmail.com>
2021-08-22 16:36:08 +0200

    :wrench: addding navBar

--- client/int-blog/pages/index.js
+++ client/int-blog/pages/index.js
@@ -1,4 +1,6 @@
+import { Box, Flex } from "@chakra-ui/react";
 import Head from "next/head";
+import MainLink from "../components/mainLink";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -16,46 +18,32 @@ export default function Home() {
         />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Box className={styles.main}>
+        <Flex alignItems='center' border='2px solid red' w="100vw" h="10vh"  flexDirection="row">
+          <nav>
+            <img
+              src="/intressantLOGO.svg"
+              alt="Interessant Logo"
+              className={styles.logo}
+            />
+            <Flex flexDirection='row'>
+              <Flex flexDirection='row' >
+                <MainLink txt='Home' fontWeight='bold' href='/'/>
+                <MainLink txt='Artigos' fontWeight='bold' href='/'/>
+                <MainLink txt='Pesquisar' fontWeight='bold' href='/'/>
+                <MainLink txt='Ajuda' fontWeight='bold' href='/'/>
+              </Flex>
+            </Flex>
+          </nav>
+        </Flex>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Flex flex="5">
+          <Flex flex="1" bg="blue"></Flex>
+          <Flex flex="3" bg="red.400"></Flex>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+          <Flex flex="1" bg="green"></Flex>
+        </Flex>
+      </Box>
 
       <footer className={styles.footer}>
         <a
@@ -64,7 +52,11 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img
+            src="/intressantLOGO.svg"
+            alt="Interessant Logo"
+            className={styles.logo}
+          />
         </a>
       </footer>
     </div>
