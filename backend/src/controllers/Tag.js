5dc801f7488ddefe5fe248a42d0e248889bf6629
caf-3 <tomas.caetano@ubi.co.mz>
2021-08-26 23:15:07 +0200

    routes bug fixed

--- backend/src/controllers/Tag.js
+++ backend/src/controllers/Tag.js
@@ -4,7 +4,18 @@ module.exports.createTag = async (req, res, next) => {
     try {
         const { name, slug } = req.body;
         if(!name) fireError({status: 401, message: "Insira o nome da tag"});
-        
+        if(!slug) fireError({status: 401, message: "Insira o link da tag"});
+        const createdTag = await Tag.create({
+            slug: slug.toLowerCase(),
+            name: name
+        })
+        if(createdTag){
+            return res.status(201).json({
+                message: "Tag criada com sucesso",
+                data: createdTag
+            })
+        }
+  
     } catch (error) {
         if(!error.statusCode) error.statusCode = 500;
         next(error);
