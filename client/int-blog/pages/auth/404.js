7af52776a76faac36949d1808835863bc95f6808
Paichato <tsenane14@gmail.com>
2021-08-29 11:36:01 +0200

    added 404 page

---
+++ client/int-blog/pages/auth/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NotFound() {
+    return (
+        <div>
+            <h1>Ooops</h1>
+            <h2>Pagina nao encontrada</h2>
+            <p>Voltar a <Link hrefef='/'><a>Pagina Inicial</a></Link> </p>
+        </div>
+    )
+}
