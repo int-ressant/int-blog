a4d040ddd2d9137720afef4c7de31270db386cef
Paichato <tsenane14@gmail.com>
2021-09-05 03:17:40 +0200

    added tags

--- client/int-blog/pages/post/create.js
+++ client/int-blog/pages/post/create.js
@@ -5,7 +5,7 @@ import {FiChevronLeft} from 'react-icons/fi'
 import {RiDraftFill} from 'react-icons/ri'
 import {IoSend} from 'react-icons/io5'
 import Image from 'next/image'
-import {Textarea} from '@chakra-ui/react'
+import {Textarea, Input, Tag, TagLabel, TagCloseButton, useToast} from '@chakra-ui/react'
 
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
@@ -18,7 +18,31 @@ const MDEditor = dynamic(
 
 export default function CreatePost() {
 
-    const [data,setData]=useState('');
+    const [data,setData]=useState('# hm...  INTressant');
+    const [tags,setTags]= useState([]);
+    const [tag,setTag]=useState('');
+    const toast=useToast();
+
+    const handleAddTag=(tagname)=>{
+        if(tags.find((tag)=>tag===tagname)){
+            console.log('error');
+            toast({
+                title:'tag ja existe',
+                status:'warning',
+                position:'top'
+            })
+        }else{ 
+        setTags(oldTags=>[...oldTags,tagname]);
+        console.log(tags);
+        }
+    }
+
+    const handleRemoveTag=(tagname)=>{
+        // const newTgas=tags;
+    //    const newTags= !tags.filter((tag)=>tag===tagname);
+  
+        setTags(oldTags=> oldTags.filter((tag)=>tag===!tagname))
+    }
 
     return (
         <Box backgroundColor='gray.100' flex='1' >
@@ -28,15 +52,40 @@ export default function CreatePost() {
             <Box w='50vw' h='100vh'>
                 <Image src={'/intressantLOGO.svg'} width={45} height={45}/>
                 <Flex flexDirection='column' >
-                    <Flex mt='5' w='100%' justifyContent='space-between' flexDirection='row'>
+                    <Flex mb='5' mt='5' w='100%' justifyContent='space-between' flexDirection='row'>
                         <Text>Criar artigo</Text>
                         <Flex>
                             
                         </Flex>
                     </Flex>
+                    {/* for tags */}
+                    <Flex>
+                        {tags.map((tagname)=>{
+                            return(<Tag
+                    size={'sm'}
+                    key={tagname}
+                    borderRadius="full"
+                    variant="solid"
+                    colorScheme="green"
+                    
+    >
+        <TagLabel>{tagname}</TagLabel>
+      <TagCloseButton onClick={()=>handleRemoveTag(tagname)} />
+    </Tag>)
+                        })}
+                    
+                    </Flex>
                     <Flex mt='5' justifyContent='space-between' flexDirection='row'>
                         
                         <Button w='120' backgroundColor='gray.200'>Adiconar Foto de capa</Button>
+                        <Box w='60%' h='100%' backgroundColor='green.100'>
+                            <Flex ml='2'>
+                                <Button onClick={()=>handleAddTag(tag)} variant='ghost' backgroundColor='transparent' >Adicionar tag</Button>
+                                <Flex  alignItems='center' w='70%' >
+                                <Input value={tag} onChange={(e)=>setTag(e.currentTarget.value)} placeholder="escreva a tag" size="sm" />
+                                </Flex>
+                            </Flex>
+                        </Box>
 
                     </Flex>
                     <Flex placeholder='something' mt='5' h='50vh' w='100%' backgroundColor='white'>
