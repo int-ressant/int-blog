ad4b9ae959ead3910c241cdb89a41d7f609075c4
Paichato <tsenane14@gmail.com>
2021-09-20 00:52:56 +0200

    added some styles

--- client/int-blog/pages/post/[slug]/single.js
+++ client/int-blog/pages/post/[slug]/single.js
@@ -61,10 +61,14 @@ api.get(`https://iblogapi.herokuapp.com/api/posts/${slug}/single`).then((res)=>{
 }
 
  return (
-  <Flex flexDirection='column' flex='1' width='100vw'>
+  <Flex p='5' flexDirection='column' flex='1' width='100vw'>
+   <Text mt='5' variant='bold' fontSize='2xl' >Title</Text>
+   <Text>{data?.data?.title}</Text>
+
+   <Text mt='5' fontSize='2xl'>Descricao</Text>
    <Text>{slug}</Text>
   
-   <MDEditor hideToolbar preview='preview'  height={'900'} width='100vw'  value={data.data.body} onChange={(e)=>setData(e)}/>
+   <MDEditor hideToolbar preview='preview'  height={'900'} width='100vw'  value={data?.data?.body} onChange={(e)=>setData(e)}/>
    {/* <MDEditor.Markdown  style={{width: '100%', height: '100vh' }} height={'100vh'}  source='hello'/> */}
   </Flex>
  )
